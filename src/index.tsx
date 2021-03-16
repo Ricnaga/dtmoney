62a789741ec3ef1f105327c55baca460194e0f28
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-16 18:35:42 -0300

    install axios | react-modal | polished - do call to fake api

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: "deposit",
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Freelancer de datascience',
+          type: "withdraw",
+          category: 'Dev',
+          amount: 2500,
+          createdAt: new Date('2021-02-14 13:00:00')
+        },
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
