ac05849b88273703ec5991628b3700d30df0bf33
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-17 13:59:10 -0300

    create contextapi

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from "./hooks/useTransactions";
 
 
 Modal.setAppElement('#root')
@@ -18,11 +19,11 @@ export function App() {
   function handleCloseNewTransactionModal() {
     setIsNewTransactionModalOpen(false)
   }
-  
+
   return (
-    <>
-      <Header 
-      onOpenNewTransactionModal={handleOpenNewTransactionModal} 
+    <TransactionsProvider>
+      <Header
+        onOpenNewTransactionModal={handleOpenNewTransactionModal}
       />
 
       <NewTransactionModal
@@ -32,9 +33,8 @@ export function App() {
 
       <Dashboard />
 
-      
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
